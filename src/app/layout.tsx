8561c89e415b8afbd98a342863cf5dc176570de9
William Santos <agi_wsantos@idcontractors.net>
2024-11-25 11:07:01 -0500

    feat(navbar): creación de navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const monserratFont = localFont({
+  src: "./fonts/MonserratFont.ttf",
+  variable: "--font-monserrat",
+  weight: "400 500 600 700",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={monserratFont.variable}>
         {children}
       </body>
     </html>
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './Navbar.module.scss';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logoContainer}>
+                <Image
+                    className={styles.logo}
+                    src="/bold-logo.svg"
+                    alt="Bold logo"
+                    width={180}
+                    height={38}
+                    priority
+                />
+            </div>
+            <div className={styles.links}>
+                <a href="#" className={styles.link}>Mi negocio</a>
+                <a href="#" className={styles.link}>Ayuda</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
